c5a11e34d457e998aea7b2c51a16da61e8459393
alsdoto <80668454+alsdoto@users.noreply.github.com>
2024-01-02 16:54:26 +0700

    Tugas 12 - PHP Lumen Client App - Json
    Tugas 12 - PHP Lumen Client App - Json

--- app/Http/Middleware/LoginMiddleware.php
+++ app/Http/Middleware/LoginMiddleware.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Http\Middleware;
 
 use Closure;
@@ -9,7 +8,7 @@ class LoginMiddleware
     public function handle($request, Closure $next)
     {
         if (!($request->input('username') == 'tedc' && $request->input('password') == 'rahasia')) {
-            return "Anda tidak di ijinkan untuk mengakses data nilai, karena username dan password anda salah.";
+            return "Anda tidak di ijinkan untuk mengakases data nilai, karena username dan password anda salah.";
         }
         return $next($request);
     }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,15 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-//     Post -> table_name = posts
-//     custome table name:
-//     protected $table='table_name'
-
-//     define cloumn name
     protected $fillable = array('title', 'content', 'status', 'user_id');
-
-//      untuk melakukan update field create_at dan update_at secara otomatis
     public $timestamps = true;
 
-}
+    public function categories()
+    {
+        return $this->belongsTo(Category::class, 'categories_id');
+    }
 
+    public function user()
+    {
+        return $this->belongsTo(User::class,'user_id');
+    }
+}
